2d7702afe0ef39a22ed79898050783cd214b70a8
maelpseudo <mael.cuny@gmail.com>
2024-01-22 16:01:08 +0100

    Updated 8 file with pycodestyle and split

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming \
 language that combines remarkable power with very clear syntax"
-str = str[39:66]
-print(str)
+str = str.split()
+print(f"{' '.join(str[5:7])} with Python")
