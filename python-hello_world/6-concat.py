e952be9d561126c6255a51d8cb3446adbc191938
maelpseudo <mael.cuny@gmail.com>
2024-01-22 15:49:51 +0100

    Updated 6 file with concat

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
+# python code test for 5 lines
 str1 = "Holberton"
 str2 = "School"
-str1 = " ".join([str1, str2])
-print(f"Welcome to {str1}!")
+str3 = " ".join([str1, str2])
+print(f"Welcome to {str3}!")
