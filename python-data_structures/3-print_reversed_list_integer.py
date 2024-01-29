a2ef3cf3a4b2e425b1b88c33a41f62826654dcf6
maelpseudo <mael.cuny@gmail.com>
2024-01-29 15:24:34 +0100

    Added Task 3 files

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@
 def print_reversed_list_integer(my_list=[]):
     nlen = len(my_list) - 1
     for i in range(nlen, -1, -1):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
