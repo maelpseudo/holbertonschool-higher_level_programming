68ec56d5b9ddf39dd0a25199f7054aa7dfb40cbd
maelpseudo <mael.cuny@gmail.com>
2024-01-29 14:30:58 +0100

    Added task 1 files

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    return (my_list[idx])
+    n = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > n:
+        return None
+    else:
+        return (my_list[idx])
