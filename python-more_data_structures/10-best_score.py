5c4131236a9bd60a054fe2b54757ad9c86542c13
maelpseudo <mael.cuny@gmail.com>
2024-01-31 18:31:55 +0100

    Added Task 10 files

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    max_key = None
+    max_value = 0
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    return max_key
