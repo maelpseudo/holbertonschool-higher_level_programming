9de1c600cb75ec60dfa6145b1ea06ab42caca750
maelpseudo <mael.cuny@gmail.com>
2024-02-01 11:19:37 +0100

    Added Task 12 files

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,18 +2,23 @@
 
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
-        return None
+        return 0
 
+    # Mapping of Roman numerals to their integer values
     roman_numerals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    
     result = 0
-    i = 0
+    prev_value = 0
 
-    while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]:
-            result += roman_numerals[roman_string[i + 1]] - roman_numerals[roman_string[i]]
-            i += 2
+    # Iterate over the string in reverse order
+    for char in reversed(roman_string):
+        value = roman_numerals[char]
+        if value < prev_value:
+            # If the current value is less than the previous one, subtract it
+            result -= value
         else:
-            result += roman_numerals[roman_string[i]]
-            i += 1
+            # Otherwise, add the current value
+            result += value
+        prev_value = value
 
     return result
