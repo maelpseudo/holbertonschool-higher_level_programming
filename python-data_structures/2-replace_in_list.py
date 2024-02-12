857e7b1d239ee37cd780059312ef2062ce28727f
maelpseudo <mael.cuny@gmail.com>
2024-02-12 17:33:50 +0100

    Added Task 0 to Task 14

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, new_element):
+def replace_in_list(my_list, idx, element):
     nlen = len(my_list) - 1
     if idx < 0:
         return my_list
     elif idx > nlen:
         return my_list
     else:
-        my_list[idx] = new_element
+        my_list[idx] = element
         return (my_list)
