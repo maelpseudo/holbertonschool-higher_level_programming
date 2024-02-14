9de8e7572ac552cc3b3d48162c435277673b6e22
maelpseudo <mael.cuny@gmail.com>
2024-02-14 22:26:50 +0100

    Updated Task 1 files

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
+""" Definition of a custom list class """
+
 
 class MyList(list):
-    """
-    MyList class that inherits from list.
-
-    Methods:
-        print_sorted(self): Prints the list in ascending order.
-    """
-
+    """  A custom list class that inherits from the built-in list class """
     def print_sorted(self):
         """
-        Prints the list in ascending order.
+        Prints the elements of the list in sorted order.
         """
         print(sorted(self))
