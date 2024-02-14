312ee2dda2f757088433f0a19ccd497cbdb10bed
maelpseudo <mael.cuny@gmail.com>
2024-02-14 22:31:38 +0100

    Updated Task 10 files

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,43 +1,42 @@
+#!/usr/bin/python3
+""" a class BaseGeometry"""
+
+
 class BaseGeometry:
-    """
-    BaseGeometry class with an unimplemented area method and integer_validator.
-    """
+    """ define a class BaseGeometry """
     def area(self):
-        raise Exception("area() is not implemented")
+        """ define area """
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
-    """
-    Rectangle class inherits from BaseGeometry.
-    """
+    """ Rectangle """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
+        """mesure area of rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
+        """define string representation"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
+
 class Square(Rectangle):
-    """
-    Square class inherits from Rectangle.
-    """
+    """square"""
+
     def __init__(self, size):
-        self.integer_validator("size", size)
-        super().__init__(size, size)  # Call the Rectangle constructor with size as both width and height
+        """Instantiation with size"""
+        super().integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        super().__init__(size, size)
