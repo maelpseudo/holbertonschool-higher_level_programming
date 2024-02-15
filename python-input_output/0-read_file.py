68b15c60f239e9d3106cdce8f8afea14a344f08a
maelpseudo <mael.cuny@gmail.com>
2024-02-15 20:34:43 +0100

    Added all task's files

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""function that reads a file"""
+
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read(), end='')
+    """Read_file"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content, end="")
