395648a5b4e358d750f5b288267ded56e216c09e
maelpseudo <mael.cuny@gmail.com>
2024-02-22 18:44:08 +0100

    Updated Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+This module defines the Rectangle class that inherits from Base.
+It includes methods for calculating the area, displaying the rectangle,
+handling string representation, updating attributes, and converting
+instance attributes to dictionary representation.
+"""
+
 from .base import Base
 
 
