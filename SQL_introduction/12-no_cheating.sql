0ee1cbbb02ea3b0ffe86b1bcdf0d18217664ef75
maelpseudo <mael.cuny@gmail.com>
2024-03-19 16:32:20 +0100

    Added task 9 to 16 files

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,2 @@
--- Ce script liste tous les enregistrements de `second_table` avec un score >= 10,
--- en affichant score et nom, et ordonne les résultats par score de manière décroissante
-
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+-- Ce script met à jour le score de Bob à 10 dans `second_table`
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
