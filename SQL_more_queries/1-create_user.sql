b213f0299683408ff4460301a2f8cf161ac0fcd5
maelpseudo <mael.cuny@gmail.com>
2024-03-20 16:11:19 +0100

    Updated task 1 file

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,8 @@
--- Crée l'utilisateur `user_0d_1` s'il n'existe pas déjà et définit son mot de passe
+-- Attempts to create the user 'user_0d_1' with the specified password if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Accorde à `user_0d_1` tous les privilèges sur le serveur MySQL
+-- Grants all privileges to 'user_0d_1' on all databases and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Applique immédiatement les modifications des privilèges
+-- Applies the changes made by the GRANT statement
 FLUSH PRIVILEGES;
