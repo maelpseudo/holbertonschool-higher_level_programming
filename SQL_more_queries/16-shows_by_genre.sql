41ad6fa0199f74577e237064b256cb2551bc4ef9
maelpseudo <mael.cuny@gmail.com>
2024-03-20 16:16:03 +0100

    Updated Task 16 file

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,6 @@
+-- listing all shows and all genres linked to that show
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name
